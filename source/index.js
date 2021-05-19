7e61c732f13796ba543d42d37428587c51c91f4a
rajsite <rajsite@users.noreply.github.com>
2021-05-19 00:09:55 -0500

    Save ms-its schema patching experiment

--- source/index.js
+++ source/index.js
@@ -1,6 +1,6 @@
 const replace = require('replace-in-file');
 
-const results = replace.sync({
+replace.sync({
     files: 'dist/**/*.{html,js,hhc,hhk}',
     from: /\w*\.chm::\//g,
     to: (content) => {
@@ -8,3 +8,17 @@ const results = replace.sync({
         return `../${result[1]}/`
     }
 });
+
+// Not sure these are actually useful
+// For now not patching so that the requests all fail for invalid schemas
+// replace.sync({
+//     files: 'dist/**/checkinstalled.js',
+//     from: `"ms-its:" + client + `,
+//     to: ''
+// });
+
+// replace.sync({
+//     files: 'dist/**/*.js',
+//     from: /ms-its:/gi,
+//     to: ''
+// });
